3a5b69c1dc829faa30aca73d167ad9452c4c7cdd
Andriy <114863893+noinsts@users.noreply.github.com>
2025-10-22 22:44:09 +0300

    feat(listeners): add TabColorListener and connect it

---
+++ src/main/java/com/noinsts/administrator/listeners/TabColorListener.java
@@ -0,0 +1,43 @@
+package com.noinsts.administrator.listeners;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class TabColorListener implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        updateColor(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onWorldChange(PlayerChangedWorldEvent event) {
+        updateColor(event.getPlayer());
+    }
+
+    @SuppressWarnings("EnhancedSwitchMigration")
+    private NamedTextColor getColor(World.Environment env) {
+        switch (env) {
+            case NETHER:
+                return NamedTextColor.RED;
+            case THE_END:
+                return NamedTextColor.LIGHT_PURPLE;
+            case NORMAL:
+            default:
+                return NamedTextColor.GREEN;
+        }
+    }
+
+    private void updateColor(Player player) {
+        World.Environment env = player.getWorld().getEnvironment();
+        NamedTextColor color = getColor(env);
+
+        player.playerListName(Component.text(player.getName()).color(color));
+    }
+}
