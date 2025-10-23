17cc70a86593136867388ae1c9e054393789f620
Andriy <114863893+noinsts@users.noreply.github.com>
2025-10-23 10:23:29 +0300

    docs(listeners): add documentation to ColoredNameListener

--- src/main/java/com/noinsts/administrator/listeners/ColoredNameListener.java
+++ src/main/java/com/noinsts/administrator/listeners/ColoredNameListener.java
@@ -10,18 +10,55 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+/**
+ * Слухач подій, що автоматично змінює колір імені в гравці залежно від виміру світу.
+ *
+ * <p>Колірна схема:</p>
+ * <ul>
+ *     <li>Звичайний світ (Overworld) - {@link NamedTextColor#GREEN зелений}</li>
+ *     <li>Пекло (Nether) - {@link NamedTextColor#RED червоний}</li>
+ *     <li>Край (The End) - {@link NamedTextColor#LIGHT_PURPLE світло-фіолетовий}</li>
+ * </ul>
+ *
+ * <p>Колір застосовується до:</p>
+ * <ul>
+ *     <li>Імені гравця в списку гравців (Tab)</li>
+ *     <li>Імені гравця в повідомленнях чату</li>
+ * </ul>
+ *
+ * @since 1.0-SNAPSHOT
+ * @author noinsts
+ */
 public class ColoredNameListener implements Listener {
 
+    /**
+     * Обробляє подію приєднання гравця до сервера.
+     * Оновлює колір імені гравця відповідно до виміру, в якому він з'явився.
+     *
+     * @param event Подія приєднання гравця.
+     */
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         updateColor(event.getPlayer());
     }
 
+    /**
+     * Обробляє подію зміни світу гравцем.
+     * Оновлює колір імені гравця при переході між вимірами.
+     *
+     * @param event Подія зміни світу.
+     */
     @EventHandler
     public void onWorldChange(PlayerChangedWorldEvent event) {
         updateColor(event.getPlayer());
     }
 
+    /**
+     * Обробляє подію відправлення сповіщення користувачем.
+     * Оновлює колір гравця у чаті залежно від виміру світу.
+     *
+     * @param event Подія відправлення сповіщення.
+     */
     @EventHandler
     public void onChat(AsyncChatEvent event) {
         Player player = event.getPlayer();
@@ -35,6 +72,18 @@ public class ColoredNameListener implements Listener {
         );
     }
 
+    /**
+     * Повертає колір залежно від виміру світу.
+     *
+     * <ul>
+     *     <li>Звичайний світ (Overworld) - {@link NamedTextColor#GREEN зелений}</li>
+     *     <li>Пекло (Nether) - {@link NamedTextColor#RED червоний}</li>
+     *     <li>Край (The End) - {@link NamedTextColor#LIGHT_PURPLE світло-фіолетовий}</li>
+     * </ul>
+     *
+     * @param env Вимір світу.
+     * @return Колір
+     */
     @SuppressWarnings("EnhancedSwitchMigration")
     private NamedTextColor getColor(World.Environment env) {
         switch (env) {
@@ -48,6 +97,10 @@ public class ColoredNameListener implements Listener {
         }
     }
 
+    /**
+     * Оновлює колір нікнейму гравця в списку гравців (Tab).
+     * @param player Гравець, якому треба змінити колір.
+     */
     private void updateColor(Player player) {
         World.Environment env = player.getWorld().getEnvironment();
         NamedTextColor color = getColor(env);
