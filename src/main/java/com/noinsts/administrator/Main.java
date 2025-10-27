2e0b315e1c197474fe41abee02bd9c29915dd544
Andriy <114863893+noinsts@users.noreply.github.com>
2025-10-27 13:39:50 +0200

    docs(core): add JavaDoc to Main class

--- src/main/java/com/noinsts/administrator/Main.java
+++ src/main/java/com/noinsts/administrator/Main.java
@@ -9,10 +9,33 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
 
+/**
+ * Головний клас плагіну "Administrator".
+ *
+ * <hr>
+ *
+ * <h1>Відповідає за:</h1>
+ *
+ * <ul>
+ *     <li>Ініціалізацію менеджерів ({@link DeathLocationManager})</li>
+ *     <li>Реєстрацію команд (/backdeath, /getcoords)</li>
+ *     <li>Реєстрацію слухачів подій ({@link PlayerDeathListener}, {@link ColoredNameListener})</li>
+ *     <li>Логування старту та вимкнення плагіну</li>
+ * </ul>
+ *
+ * @since 1.0-SNAPSHOT
+ * @author noinsts
+ */
 public final class Main extends JavaPlugin {
 
+    /** Менеджер, який зберігає координати останньої смерті гравців */
     private DeathLocationManager deathLocationManager;
 
+    /**
+     * Метод, який викликається при ініціалізації плагіну.
+     * <p>
+     * Тут відбувається ініціалізація менеджерів, команд та слухачів подій.
+     */
     @Override
     public void onEnable() {
         getLogger().info("⚙️ Administrator plugin is starting...");
@@ -24,20 +47,34 @@ public final class Main extends JavaPlugin {
         getLogger().info("✅ Administrator plugin successfully enabled!");
     }
 
+    /**
+     * Метод, який викликається при вимкненні плагіну.
+     * <p>
+     * Використовується для логування завершення роботи плагіну.
+     */
     @Override
     public void onDisable() {
         getLogger().info("\uD83D\uDED1 Administrator plugin disabled.");
     }
 
+    /**
+     * Ініціалізує всі менеджери плагіну
+     */
     private void initializeManagers() {
         this.deathLocationManager = new DeathLocationManager();
     }
 
+    /**
+     * Реєструє всі команди плагіну
+     */
     private void registerCommands() {
         Objects.requireNonNull(this.getCommand("getcoords")).setExecutor(new GetCoordsCommand());
         Objects.requireNonNull(this.getCommand("backdeath")).setExecutor(new BackDeathCommand(deathLocationManager));
     }
 
+    /**
+     * Реєструє всі слухачі подій плагіну
+     */
     private void registerListeners() {
         var pm = getServer().getPluginManager();
         pm.registerEvents(new ColoredNameListener(), this);
