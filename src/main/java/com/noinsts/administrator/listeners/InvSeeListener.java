a4ecf5bf4082dab1d4aedf48d94a23f5c0646222
Andriy <114863893+noinsts@users.noreply.github.com>
2025-10-29 23:43:51 +0200

    docs(listeners): add JavaDoc in InvSeeListener

--- src/main/java/com/noinsts/administrator/listeners/InvSeeListener.java
+++ src/main/java/com/noinsts/administrator/listeners/InvSeeListener.java
@@ -6,8 +6,30 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
 
+/**
+ * Слухай подій пов'язаних з командою {@code invsee}.
+ *
+ * <p>
+ *
+ * Забороняє будь-яку взаємодію з інвентарем, відкритим через команду
+ * {@code invsee}, щоб гравець не міг брати речі з чужого інвентарю.
+ *
+ * <p>
+ *
+ * Виконує блокування кліків і перетягувань предметів у вікнах,
+ * заголовок яких починається з {@code "Інвентар "}.
+ *
+ * @since 1.1-SNAPSHOT
+ * @author noinsts
+ */
 public class InvSeeListener implements Listener {
 
+    /**
+     * Обробляє події кліку в інвентарі та блокує будь-яку взаємодію
+     * у вікнах, відкритих через команду {@code invsee}.
+     *
+     * @param event Подія {@link InventoryClickEvent}, що виникає при кліку в інвентарі.
+     */
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
         String title = PlainTextComponentSerializer.plainText().serialize(event.getView().title());
@@ -16,6 +38,12 @@ public class InvSeeListener implements Listener {
         }
     }
 
+    /**
+     * Обробляє події перетягування предметів у інвентарі та блокує
+     * будь-які зміни в інвентарях, відкритих через команду {@code invsee}.
+     *
+     * @param event Подія {@link InventoryDragEvent}, що виникає при перетягуванні предметів.
+     */
     @EventHandler
     public void onInventoryDrag(InventoryDragEvent event) {
         String title = PlainTextComponentSerializer.plainText().serialize(event.getView().title());
