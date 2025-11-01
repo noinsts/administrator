79308d834d1f7edf226dff13b7bcc3ed9b5cc740
Andriy <114863893+noinsts@users.noreply.github.com>
2025-11-01 11:01:28 +0200

    feat(utils): add sendError and ERROR_PREFIX in MessageUtil

--- src/main/java/com/noinsts/administrator/utils/MessageUtil.java
+++ src/main/java/com/noinsts/administrator/utils/MessageUtil.java
@@ -1,5 +1,11 @@
 package com.noinsts.administrator.utils;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Utility клас для форматування та відправки повідомлень гравцям.
  *
@@ -8,10 +14,30 @@ package com.noinsts.administrator.utils;
  */
 public final class MessageUtil {
 
+    private static final String ERROR_PREFIX = "❌ ";
+
     /**
      * Приватний конструктор для запобігання створенню екземплярів.
      */
     private MessageUtil() {
         throw new UnsupportedOperationException("Utility class cannot be instantiated");
+    }
+
+    /**
+     * Відправляє повідомлення про помилку.
+     *
+     * @param sender Отримувач повідомлення.
+     * @param message Текст помилка.
+     */
+    public static void sendError(
+            @NotNull final CommandSender sender,
+            @NotNull final String message
+    ) {
+        final Component component = Component.text()
+                .append(Component.text(ERROR_PREFIX, NamedTextColor.RED))
+                .append(Component.text(message, NamedTextColor.RED))
+                .build();
+
+        sender.sendMessage(component);
     }
 }
