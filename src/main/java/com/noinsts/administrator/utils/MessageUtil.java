b21806e4f6457515f54cce278bf1d75a9cbac0a6
Andriy <114863893+noinsts@users.noreply.github.com>
2025-11-01 11:00:34 +0200

    feat(utils): add MessageUtil utility class

---
+++ src/main/java/com/noinsts/administrator/utils/MessageUtil.java
@@ -0,0 +1,17 @@
+package com.noinsts.administrator.utils;
+
+/**
+ * Utility клас для форматування та відправки повідомлень гравцям.
+ *
+ * @since 1.1-SNAPSHOT
+ * @author noinsts
+ */
+public final class MessageUtil {
+
+    /**
+     * Приватний конструктор для запобігання створенню екземплярів.
+     */
+    private MessageUtil() {
+        throw new UnsupportedOperationException("Utility class cannot be instantiated");
+    }
+}
